bad028538ce4f07affcc70aa82ac42fb4c512926
Sakchham Sangroula <ssangroula10@outlook.com>
2025-01-20 09:13:36 -0600

    Adding not operator

--- vm.c
+++ vm.c
@@ -37,6 +37,9 @@ Value pop() {
 }
 
 static Value peek(int distance) { return vm.stackTop[-1 - distance]; }
+static bool isFalsey(Value value) {
+  return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
 
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
@@ -94,6 +97,8 @@ static InterpretResult run() {
       BINARY_OP(NUMBER_VAL, /);
       break;
     }
+    case OP_NOT:
+      push(BOOL_VAL(isFalsey(pop())));
     case OP_NEGATE: {
       if (!IS_NUMBER(peek(0))) {
         runtimeError("Operand must be a number.");
