580de6ca3d7db84fade9b42fd5ab48be1b31cfe5
Sakchham Sangroula <ssangroula10@outlook.com>
2025-03-12 06:34:55 -0500

    Adding call frames to the vm and using function instead of chunks

--- object.h
+++ object.h
@@ -6,16 +6,17 @@
 #include "value.h"
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
-#define IS_STRING(value) isObjType(value, OBJ_STRING)
+
 #define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
+#define IS_STRING(value) isObjType(value, OBJ_STRING)
 
 #define AS_FUNCTION(value) ((ObjFunction *)AS_OBJ(value))
 #define AS_STRING(value) ((ObjString *)AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString *)AS_OBJ(value))->chars)
 
 typedef enum {
-  OBJ_STRING,
   OBJ_FUNCTION,
+  OBJ_STRING,
 } ObjType;
 
 struct Obj {
